4872e3cdeb68267a6152980b83c691d7cf3f961d
madanmohan333 <shivaclasses406@gmail.com>
2026-02-18 14:22:19 +0530

    basic server started

---
+++ db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+const connectDB = async () => {
+  try {
+    const mongodbURI = process.env.MONGO_URI;
+
+
+    await mongoose.connect(mongodbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
+    process.exit(1); // Exit the process with an error
+  }
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const express=require("express");
 const app=express();
+const connectDB=require("./db");
+const port=process.env.PORT || 5000;
+
+
+
+
+
+connectDB()
+.then(()=>{
+    console.log("database is connected");
+    app.listen(port,()=>{
+        console.log(`server is successfully listening at port ${port}`);
+    })
+})
+.catch((err)=>{
+      console.log("database can not be connected!!")
+})
