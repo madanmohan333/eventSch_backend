547fa8eea375558b161a24967ce93512d46dbe34
madanmohan333 <shivaclasses406@gmail.com>
2026-02-18 16:24:00 +0530

    mail api created

---
+++ env.js
@@ -0,0 +1,4 @@
+module.exports={
+    EMAIL:"bemadanmohan@gmail.com",
+    PASSWORD:"rzgfklgaphuecink"
+}
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 require('dotenv').config();
 const express=require("express");
 const app=express();
-// const cors = require("cors");
+const cors = require("cors");
 const connectDB=require("./db");
-// const path = require('path');
+const path = require('path');
 const port=process.env.PORT || 5000;
+const mailRoute = require('./routes/mail')
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/event", require("./routes/event"));
-// app.use("/api/chat", require("./routes/chats"));
-// app.use("/api/meet", require("./routes/meets"));
-// app.use("/api/sendmail", mailRoute);
+app.use("/api/chat", require("./routes/chats"));
+app.use("/api/meet", require("./routes/meets"));
+app.use("/api/sendmail", mailRoute);
 
 
 
