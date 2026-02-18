369903a7c19d92a14c1505e8ed225c1fb84417a9
madanmohan333 <shivaclasses406@gmail.com>
2026-02-18 14:42:36 +0530

    event schema updated

--- models/events.js
+++ models/events.js
@@ -11,3 +11,47 @@ const userSchema = new Schema({
   photo: String,
   username: String,
 });
+const taskSchema = new Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const eventsSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  eventStartDate: String,
+  eventEndDate: String,
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  collaborators: [userSchema],
+  admin: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  tasks: [taskSchema],
+  date_created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const EventsModel = mongoose.model("Event", eventsSchema);
+module.exports = EventsModel;
